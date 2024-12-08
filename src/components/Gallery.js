57216043af3ac3a7ab3fe96ab1d19309dc954c81
Lolluckt <egorbl2004@gmail.com>
2024-12-08 18:56:36 +0200

    Com

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -3,14 +3,13 @@ import GoodsCard from './GoodsCard';
 
 const Gallery = () => {
     const goods = [
-        { id: 1, image: './images/1.jpg', name: 'Товар 1', price: 200 },
-        { id: 2, image: './images/2.jpg', name: 'Товар 2', price: 300 },
-        { id: 3, image: './images/3.jpg', name: 'Товар 3', price: 400 },
-        { id: 4, image: './images/4.jpg', name: 'Товар 4', price: 500 },
-        { id: 5, image: './images/5.jpg', name: 'Товар 5', price: 600 },
-        { id: 6, image: './images/6.jpg', name: 'Товар 6', price: 700 },
+        { id: 1, image: 'https://lolluckt.github.io/FrontLab7/images/1.jpg', name: 'Товар 1', price: 200 },
+        { id: 2, image: 'https://lolluckt.github.io/FrontLab7/images/2.jpg', name: 'Товар 2', price: 300 },
+        { id: 3, image: 'https://lolluckt.github.io/FrontLab7/images/3.jpg', name: 'Товар 3', price: 400 },
+        { id: 4, image: 'https://lolluckt.github.io/FrontLab7/images/4.jpg', name: 'Товар 4', price: 500 },
+        { id: 5, image: 'https://lolluckt.github.io/FrontLab7/images/5.jpg', name: 'Товар 5', price: 600 },
+        { id: 6, image: 'https://lolluckt.github.io/FrontLab7/images/6.jpg', name: 'Товар 6', price: 700 },
     ];
-
     return (
         <div className="gallery">
             {goods.map((item) => (
